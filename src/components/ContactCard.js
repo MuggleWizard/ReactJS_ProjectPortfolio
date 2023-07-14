39c85b957c019bb765fae417d10803092f9a03c3
Darlene Kartali <Cathat9@aol.com>
2023-07-14 13:19:39 -0500

    removed Link component

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import user from "../images/user.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 
 const ContactCard = (props) => {
@@ -10,10 +10,9 @@ const ContactCard = (props) => {
         <div className ="item">
             <img className="ui avatar image" src={user} alt="user"/>
             <div className = "content">
-                <Link to={{pathname:`/contact/${id}`, state:{contact: props}}}>
-                    <div className ="header">{name}</div>
-                    <div>{email}</div>
-                </Link>
+                <div className ="header">{name}</div>
+                <div>{email}</div>
+              
             </div>
             <i
             className = "trash alternate outline icon" 
